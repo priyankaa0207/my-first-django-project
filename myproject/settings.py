1f83320f137ef06c44d2b4e9f96417867b325188
priyanka <priyanka.maheshwari@newgenapps.com>
2017-12-20 18:25:10 +0530

    changed the html for the site

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(6sx&x2m%jwd=tujt)lh-+0+c5#ovu21+oy9(4u5b2k(0$8-b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'http://priyankaa0207.pythonanywhere.com/']
 
