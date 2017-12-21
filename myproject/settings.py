65f7941466929bde25c15fe95baa3bc39819bab8
priyanka <priyanka.maheshwari@newgenapps.com>
2017-12-21 16:10:51 +0530

    git project name add in settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(6sx&x2m%jwd=tujt)lh-+0+c5#ovu21+oy9(4u5b2k(0$8-b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'http://priyankaa0207.pythonanywhere.com/']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'my-first-django-project',
 ]
 
 MIDDLEWARE = [
